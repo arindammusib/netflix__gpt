70ac2ca65a914696e120464f98ff6a7865c4a414
Arindam <amusib32@gmail.com>
2024-03-03 14:01:10 +0530

    api handling

--- src/common/openai.js
+++ src/common/openai.js
@@ -2,7 +2,7 @@ import OpenAI from 'openai';
 import { OPENAI_KEY } from './constants';
 
 const openai = new OpenAI({
-  apiKey: OPENAI_KEY,
+  apiKey: process.env.REACT_APP_OPENAI_KEY,
   dangerouslyAllowBrowser: true 
 });
 export default openai;
